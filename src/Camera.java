5f56485e5bb3807143c690cf7ddd369033d90189
steven alvin <stevenalvin1805@gmail.com>
2019-04-10 15:23:42 +1000

    first time initialise

---
+++ src/Camera.java
@@ -0,0 +1,26 @@
+
+/**
+ * This class should be used to restrict the game's view to a subset of the entire world.
+ * 
+ * You are free to make ANY modifications you see fit.
+ * These classes are provided simply as a starting point. You are not strictly required to use them.
+ */
+public class Camera {
+
+	public float getLeft() {
+		// You probably want to change this.
+		return 0;
+	}
+	public float getTop() {
+		// You probably want to change this.
+		return 0;
+	}
+	public float getRight() {
+		// You probably want to change this.
+		return 0;
+	}
+	public float getBottom() {
+		// You probably want to change this.
+		return 0;
+	}
+}
---
+++ src/World.java
@@ -0,0 +1,18 @@
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+
+/**
+ * This class should be used to contain all the different objects in your game world, and schedule their interactions.
+ * 
+ * You are free to make ANY modifications you see fit.
+ * These classes are provided simply as a starting point. You are not strictly required to use them.
+ */
+public class World {
+	public void update(Input input, int delta) {
+		
+	}
+	
+	public void render(Graphics g) {
+		
+	}
+}
